6c7df973d3fae8b1bb31e652e3817deac31feec7
Jakub Warmuz <jakub.warmuz@gmail.com>
2009-08-31 03:04:38 +0200

    Client now operates in two modes: ssh or cryptcat!

--- client.sh
+++ client.sh
@@ -1,8 +1,23 @@
-#/bin/bash
-# client.sh
+#!/bin/bash
 
-HOST='s'
-PORT=1025
-PASS='test'
+usage="USAGE:\n\t`basename $0` ssh <standard arguments apply>\n\t`basename $0` cryptcat <host> <port> <password>"
 
-cryptcat -k $PASS $HOST $PORT  | sed -u -e "s/'/'\\\\''/g" | sed -u -e "s/\(.*\)\t\(.*\)/notify-send '\1' '\2' -i notification-message-im/" | bash
+if [[ $1 == 'ssh' ]]
+then
+    # Using SSH
+    command="$* -t cat notify | sed -u -e 's/\\r//'"
+elif [[ $1 == 'cryptcat' && $# == 4 ]]
+then
+    # Using cryptcat
+    command="cryptcat -k $4 $2 $3"
+else
+    echo -e $usage
+    exit
+fi
+
+eval $command | sed -u -n -e "s/\r//" -e "s/'/'\\\\''/g" -e "s/^\(.*\)\t\(.*\)$/notify-send '\1' '\2' -i notification-message-im/p" | bash
+
+if [[ $? -eq 0 && $1 == 'cryptcat' ]]
+then
+    echo "Is cryptcat-server.sh running?"
+fi
